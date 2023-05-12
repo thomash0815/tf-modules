27d26674d405c16869e0fcfd76434cdad00ba577
Thomas Hafermalz <thomas.hafermalz@accenture.com>
2023-05-12 17:51:30 +0200

    simple change for demo

--- webapp/main.tf
+++ webapp/main.tf
@@ -4,7 +4,9 @@ resource "azurerm_service_plan" "appserviceplan" {
   location                     = var.location
   os_type                      = var.os_type
   sku_name                     = var.sku_name
-  tags                         = var.tags
+  tags                         = {
+    environment = "Production"
+  }
 }
 
 
