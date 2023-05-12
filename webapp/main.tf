102cfd5059ce27ae7170456e0167f3e287c1613b
Thomas Hafermalz <thomas.hafermalz@accenture.com>
2023-05-12 18:05:11 +0200

    change back

--- webapp/main.tf
+++ webapp/main.tf
@@ -1,22 +1,20 @@
 resource "azurerm_service_plan" "appserviceplan" {
-  name                         = var.appserviceplan_name
-  resource_group_name          = var.resource_group_name
-  location                     = var.location
-  os_type                      = var.os_type
-  sku_name                     = var.sku_name
-  tags                         = {
-    environment = "Production"
-  }
+  name                = var.appserviceplan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type
+  sku_name            = var.sku_name
+  tags                = var.tags
 }
 
 
 resource "azurerm_linux_web_app" "webapp" {
-  name                  = var.webapp_name
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = var.https_only
-  tags                  = var.tags
+  name                = var.webapp_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  https_only          = var.https_only
+  tags                = var.tags
 
   site_config {
     ftps_state = var.ftps_state
@@ -26,7 +24,7 @@ resource "azurerm_linux_web_app" "webapp" {
     type = var.identity_type
   }
 
-  dynamic storage_account {
+  dynamic "storage_account" {
     for_each = var.storage_account.name == "" ? [] : [1]
     content {
       name         = var.storage_account.name
